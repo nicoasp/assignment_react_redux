3b5c629b54637f237480cb0e7d848f302c3a8c27
nicoasp <nicolas.amaya@gmail.com>
2017-05-15 18:31:42 +0200

    working on show grocery list

---
+++ grocery_list/src/components/ItemCard.js
@@ -0,0 +1,33 @@
+import React, {PropTypes} from 'react'
+// import AdoptButton from './AdoptButton'
+
+// Custom card component for each item's data
+const ItemCard = ({item, onPurchaseClick}) => {
+  const {name, description, amount, category, purchased} = item
+
+  // Using Bootstrap 4 card classes
+  return (
+    <div
+      className="ItemCard card"
+      style={{maxWidth: '320px'}}
+    >
+	    <div className="card-block">
+	      <h4>{name}</h4>
+	      <p>{description}</p>
+	      <p>Amount: {amount}</p>
+	      <p>Category: {category}</p>
+	      <PurchaseButton
+	        purchased={purchased}
+	        onPurchaseClick={onPurchaseClick}
+	      />
+	    </div>
+    </div>
+  )
+}
+
+ItemCard.propTypes = {
+  Item: PropTypes.object.isRequired,
+  onPurchaseClick: PropTypes.func.isRequired,
+}
+
+export default ItemCard
